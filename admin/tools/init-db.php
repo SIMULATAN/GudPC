dd3d18fd64167c836b30ef9d893591d913b6b079
SIMULATAN <simulatan2319@gmail.com>
2023-05-29 21:52:05 +0200

    Move database initialization into .sql file

--- admin/tools/init-db.php
+++ admin/tools/init-db.php
@@ -1,68 +1,32 @@
 <?php
+	class Migrator
+	{
+		# const in a class because shitty ass php doesn't allow final variables outside of classes lmao
+		const currentVersion = 1;
+	}
+
+	# I despise PHP for this like what the fuck
+	require_once "../../config/config.php";
+
+	global $config;
+
 	global $dbconn;
-	$dbconn = pg_connect("host=postgres port=5432 dbname=db user=app password=app");
+	$dbconn = pg_connect($config->db_connection_string);
 
-	pg_exec($dbconn, "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY,
-		username VARCHAR(255) NOT NULL UNIQUE,
-		password VARCHAR(255) NOT NULL,
-		email VARCHAR(255) NOT NULL UNIQUE,
-		created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-		profile_picture VARCHAR(255)
-	);");
-
-	// CREATE IF NOT EXISTS is not supported in PostgreSQL (lmao)
-	pg_exec($dbconn, "
-		DO $$ BEGIN
-			CREATE TYPE storage_type AS ENUM ('SSD', 'HDD');
-		EXCEPTION
-			WHEN duplicate_object THEN null;
-		END $$;
-	");
-
-	pg_exec($dbconn, "CREATE TABLE IF NOT EXISTS storage (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL UNIQUE,
-		type storage_type NOT NULL,
-		price INT NOT NULL CHECK (price > 0),
-		capacity INT NOT NULL CHECK (capacity > 0)
-	);");
-
-	pg_exec($dbconn, "CREATE TABLE IF NOT EXISTS cpu (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL UNIQUE,
-		price INT NOT NULL CHECK (price > 0),
-		cores INT NOT NULL CHECK (cores > 0),
-		threads INT NOT NULL CHECK (threads > 0),
-		frequency INT NOT NULL CHECK (frequency > 0)
-	);");
-
-	pg_exec($dbconn, "CREATE TABLE IF NOT EXISTS gpu (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL UNIQUE,
-		chipset VARCHAR(255),
-		price INT NOT NULL CHECK (price > 0),
-		memory INT NOT NULL CHECK (memory > 0),
-		frequency INT NOT NULL CHECK (frequency > 0)
-	);");
-
-	pg_exec($dbconn, "CREATE TABLE IF NOT EXISTS motherboard (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL UNIQUE,
-		price INT NOT NULL CHECK (price > 0),
-		memory_slots INT NOT NULL CHECK (memory_slots > 0),
-		memory_max INT NOT NULL CHECK (memory_max > 0)
-	)");
-
-	pg_exec($dbconn, "CREATE TABLE IF NOT EXISTS ram (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(255) NOT NULL UNIQUE,
-		type VARCHAR(255) NOT NULL,
-		frequency INT NOT NULL CHECK (frequency > 0),
-		capacity INT NOT NULL CHECK (capacity > 0),
-		rgb BOOLEAN NOT NULL,
-		price INT NOT NULL CHECK (price > 0)
-	);");
-
-	echo "Database initialized.\n";
-?>
+	// create tables
+	$create_tables = file_get_contents("sql/create.sql");
+	// begin transaction
+	pg_query($dbconn, "BEGIN");
+	$result = pg_query($dbconn, $create_tables);
+	// check if it worked
+	if ($result) {
+		echo "Database initialized - inserting version....\n";
+		$newVersion = Migrator::currentVersion;
+		pg_exec($dbconn, "INSERT INTO migrations VALUES ($newVersion)");
+		echo "Migration version inserted :) comitting now...\n";
+		pg_exec($dbconn, "COMMIT");
+		echo "Commit done les gooo\n";
+	} else {
+		echo "\nDatabase initialization FAILED - make sure this truly is the first ever run!\n";
+		pg_exec($dbconn, "ROLLBACK");
+	}
---
+++ admin/tools/sql/create.sql
@@ -0,0 +1,68 @@
+CREATE TABLE users
+(
+    id              SERIAL PRIMARY KEY,
+    username        VARCHAR(255) NOT NULL UNIQUE,
+    password        VARCHAR(255) NOT NULL,
+    email           VARCHAR(255) NOT NULL UNIQUE,
+    created_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    profile_picture VARCHAR(255)
+);
+
+CREATE TYPE storage_type AS ENUM ('SSD', 'HDD');
+
+CREATE TABLE storage
+(
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL UNIQUE,
+    type     storage_type NOT NULL,
+    price    INT          NOT NULL CHECK (price > 0),
+    capacity INT          NOT NULL CHECK (capacity > 0)
+);
+
+CREATE TABLE cpu
+(
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL UNIQUE,
+    price     INT          NOT NULL CHECK (price > 0),
+    cores     INT          NOT NULL CHECK (cores > 0),
+    threads   INT          NOT NULL CHECK (threads > 0),
+    frequency INT          NOT NULL CHECK (frequency > 0)
+);
+
+CREATE TABLE gpu
+(
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL UNIQUE,
+    chipset   VARCHAR(255),
+    price     INT          NOT NULL CHECK (price > 0),
+    memory    INT          NOT NULL CHECK (memory > 0),
+    frequency INT          NOT NULL CHECK (frequency > 0)
+);
+
+
+CREATE TABLE motherboard
+(
+    id           SERIAL PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    price        INT          NOT NULL CHECK (price > 0),
+    memory_slots INT          NOT NULL CHECK (memory_slots > 0),
+    memory_max   INT          NOT NULL CHECK (memory_max > 0)
+);
+
+CREATE TABLE ram
+(
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL UNIQUE,
+    type      VARCHAR(255) NOT NULL,
+    frequency INT          NOT NULL CHECK (frequency > 0),
+    capacity  INT          NOT NULL CHECK (capacity > 0),
+    rgb       BOOLEAN      NOT NULL,
+    price     INT          NOT NULL CHECK (price > 0)
+);
+
+CREATE TABLE migrations
+(
+    version INTEGER PRIMARY KEY,
+    date   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
