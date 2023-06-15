837ac21d5479a7e90f1ab5bc55eb979050ca283d
SIMULATAN <simulatan2319@gmail.com>
2023-06-15 17:02:09 +0200

    Remove root_url and root from config

--- config/config.php
+++ config/config.php
@@ -1,8 +1,6 @@
 <?php
 	class Config {
 		public string $root_path;
-		public string $root_url;
-		public string $root;
 		public string $db_connection_string;
 	}
 
@@ -10,8 +8,6 @@
 	$config = new Config();
 	$ini = parse_ini_file("config.ini");
 	$config->root_path = $ini["root_path"];
-	$config->root_url = $ini["root_url"];
-	$config->root = $config->root_url . $config->root_path;
 	$config->db_connection_string = $ini["db_connection_string"];
 
 	global $config;
